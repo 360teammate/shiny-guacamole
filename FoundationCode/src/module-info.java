156bdaaff07d6a952fc3b23fdbafc8262dd4264d
Abram <58718191+4b3c@users.noreply.github.com>
2025-04-04 19:04:28 -0700

    make junit static so you run main without and tests with

--- FoundationCode/src/module-info.java
+++ FoundationCode/src/module-info.java
@@ -2,7 +2,7 @@ module FoundationCode {
     requires javafx.controls;
     requires java.sql;
     requires javafx.graphics;
-    requires junit;
+    requires static junit;
 
     opens Application to javafx.graphics, javafx.fxml;
     opens Testing;
